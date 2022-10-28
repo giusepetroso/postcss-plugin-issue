caf7f7e5e5576b1901491a979f2eadb8f751ff01
Giuse Petroso <g.petroso@gmail.com>
2022-10-28 17:29:06 +0200

    removed postcss-conditionals

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,12 @@
+const path = require("path");
 module.exports = (ctx) => ({
   plugins: {
     "postcss-import": {},
     "postcss-at-rules-variables": {},
     "postcss-each": {},
-    "postcss-mixins": {},
-    "postcss-conditionals": {},
-    "postcss-simple-vars": {},
+    "postcss-mixins": {
+      mixinsDir: path.join(__dirname, "src/assets/components/mixins"),
+    },
     "tailwindcss/nesting": {},
     tailwindcss: {},
     autoprefixer: {},
